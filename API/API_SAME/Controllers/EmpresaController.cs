6f10d2689526e9616690d7f10a897c8f2bdc4693
hector cruz <cruz_har@hotmail.es>
2024-06-01 13:45:13 -0600

    API SAME

---
+++ API/API_SAME/Controllers/EmpresaController.cs
@@ -0,0 +1,69 @@
+﻿using API_SAME.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using System.Data.SqlClient;
+
+namespace API_SAME.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaController : ControllerBase
+    {
+
+        public readonly string con;
+
+        public EmpresaController(IConfiguration configuration)
+        {
+            con = configuration.GetConnectionString("conexion");
+        }
+
+
+        [HttpGet]
+
+        public IEnumerable<Empresa> Post(string nombre ="")
+        {
+
+            List<Empresa> empresas = new();
+
+            using(SqlConnection connection = new SqlConnection(con))
+            {
+
+                connection.Open();
+                using(SqlCommand cmd= new("TB_EMPRESA_L_SP", connection))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    //cmd.Parameters.AddWithValue("@NOMBRE", nombre);
+
+                    string nombreParametro = string.IsNullOrEmpty(nombre)? string.Empty : nombre;
+                    cmd.Parameters.AddWithValue("@NOMBRE", nombreParametro);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Empresa e = new Empresa
+                            {
+                                idEmpresa = reader["ID_EMPRESA"] == DBNull.Value ? 0 : Convert.ToInt32(reader["ID_EMPRESA"]),
+                                nombreEmpresa = reader["NOMBRE_EMPRESA"] == DBNull.Value ? string.Empty : reader["NOMBRE_EMPRESA"].ToString(),
+                                direccionEmpresa = reader["DIRECCION_EMPRESA"] == DBNull.Value ? string.Empty : reader["DIRECCION_EMPRESA"].ToString(),
+                                telefonoEmpresa = reader["TELEFONO_EMPRESA"] == DBNull.Value ? string.Empty : reader["TELEFONO_EMPRESA"].ToString()
+
+                            };
+                            empresas.Add(e);
+
+                        }
+
+                    }
+                }
+            }
+            return empresas;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ API/API_SAME/Models/Empresa.cs
@@ -0,0 +1,15 @@
+﻿namespace API_SAME.Models
+{
+    public class Empresa
+    {
+        public int idEmpresa { get; set; }
+        public string nombreEmpresa { get; set; }
+        public string direccionEmpresa { get; set; }
+        public string telefonoEmpresa { get; set; }
+
+
+
+
+
+    }
+}
